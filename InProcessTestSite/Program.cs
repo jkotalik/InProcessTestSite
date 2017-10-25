d8c2aec0bd3f15e955c106536aa3176f9de07a48
Justin Kotalik <jukotali@microsoft.com>
2017-10-24 17:44:38 -0700

    Adds MVC app

---
+++ InProcessTestSite/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace InProcessTestSite
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            new WebHostBuilder()
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
